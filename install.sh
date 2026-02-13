2026082a959bc80eecd77b319d51c62ea908c7db
undeadpelmen <voroshilin.andrey.08@gmail.com>
2026-02-13 18:39:39 +0300

    ЫЫЫЫЫЫЫЫЫЫыыыыыыыыыыыыыыЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫы

--- install.sh
+++ install.sh
@@ -6,12 +6,12 @@ go mod tidy
 
 go get
 
-go build -o ./main.go terarrium-app
+go build -o terarrium-app .
 
 sudo mv terarrium-app /opt/terarrium-app
 
 sudo cp ./terarrium.service /etc/systemd/system/terarrium.service
-
+11
 sudo systemctl daemon-reload
 sudo systemctl enable terrarium.service
 sudo systemctl start terrarium.service
